119d066175cdeca576c9f335a32615b5b71038c6
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2022-10-29 11:01:21 +0200

    initial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,209 @@
+import java.io.File
+import java.io.FileInputStream
+import java.nio.ByteBuffer
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.Files
+
+fun main(args: Array<String>) {
+    val file = File("javagdb")
+    val loader = ElfHeaderLoader.elfLoader(file)
+    loader?.section(".shstrtab")?.let {
+        val dump = (it as? ElfHeaderLoader.ElfStrTabSection)?.dumpTable()
+        dump?.forEach {
+            println(it)
+        }
+    }
+    loader?.section(".debug.svm.imagebuild.arguments")?.let {
+        val dump = loader.ElfStrTabSection(it as ElfHeaderLoader.ElfProgBitsSectionHeader).dumpTable()
+        dump.forEach {
+            println(it)
+        }
+    }
+    loader?.section(".debug.svm.imagebuild.java.properties")?.let {
+        val dump = loader.ElfStrTabSection(it as ElfHeaderLoader.ElfProgBitsSectionHeader).dumpTable()
+        dump.forEach {
+            println(it)
+        }
+    }
+
+}
+
+class ElfHeaderLoader(val bitness: BitnessHeaderOffsets, val buffer: MappedByteBuffer) {
+
+    companion object {
+        fun elfLoader(file: File): ElfHeaderLoader? {
+            return FileInputStream(file).use {
+                val elfHeader = ByteArray(16)
+                val byteBuffer = it.channel.map(FileChannel.MapMode.READ_ONLY, 0, Files.size(file.toPath()))
+                byteBuffer.get(elfHeader)
+                if (elfHeader[0] == 0x7f.toByte() &&
+                    elfHeader[1] == 'E'.code.toByte() &&
+                    elfHeader[2] == 'L'.code.toByte() &&
+                    elfHeader[3] == 'F'.code.toByte()
+                ) {
+                    byteBuffer.position(0)
+                    return@use when (elfHeader[ElfHeaderLoader.BitnessHeaderOffsets.BITNESS_32.bitnessOffset].toInt()) {
+                        1 -> ElfHeaderLoader(ElfHeaderLoader.BitnessHeaderOffsets.BITNESS_32, byteBuffer)
+                        2 -> ElfHeaderLoader(ElfHeaderLoader.BitnessHeaderOffsets.BITNESS_64, byteBuffer)
+                        else -> TODO()
+                    }
+                }
+                null
+            }
+        }
+    }
+
+    enum class BitnessHeaderOffsets(
+        val offsetEntrySize: Int,
+        val bitnessOffset: Int,
+        val endianess: Int,
+        val type: Int,
+        val machine: Int,
+        val entry: Int,
+        val programHeaderOffset: Int,
+        val sectionHeaderOffset: Int,
+        val elfHeaderSize: Int,
+        val programHeaderEntrySize: Int,
+        val programHeaderEntryNum: Int,
+        val sectionHeaderEntrySize: Int,
+        val sectionHeaderNumber: Int,
+        val sectionHeaderStringIndex:Int
+        ){
+        BITNESS_32(4, 0x4, 0x5, 0x10, 0x12, 0x18,
+            0x1c, 0x20, 0x20, 0x2a,
+            0x2c, 0x2e, 0x30, 0x32),
+        BITNESS_64(8, 0x4, 0x5, 0x10, 0x12, 0x18,
+            0x20, 0x28, 0x28, 0x36,
+            0x38, 0x3a, 0x3c, 0x3e)
+
+    }
+    val fullElfHeader: ByteArray by lazy {
+        buffer.position(0)
+        ByteArray(readShort(bitness.elfHeaderSize.toULong()).toInt()).also {
+            buffer.position(0)
+            buffer.get(it)
+        }
+    }
+
+    private val sectionHeaderOffset = readULong(bitness.sectionHeaderOffset.toULong())
+    private val sectionHeaderEntrySize = readShort(bitness.sectionHeaderEntrySize.toULong())
+    private val sectionHeaderStringIndex = readShort(bitness.sectionHeaderStringIndex.toULong())
+
+    private val sectionHeaderStringTable by lazy {
+        section(sectionHeaderStringIndex.toInt()) as ElfStrTabSection
+    }
+
+    fun readShort(offset: ULong): Short {
+        return ByteArray(2).let {
+            buffer.atOffset(offset) {
+                get(it)
+                it[1].toUByte().toInt().shl(8).or(it[0].toUByte().toInt())
+            }
+        }.toShort()
+    }
+
+    fun readUInt(offset: ULong): UInt {
+        return ByteArray(4).let {
+            buffer.atOffset(offset) {
+                buffer.get(it)
+                var value = 0U
+                for (i in 0 until 4) {
+                    value = value.or(it[i].toUByte().toUInt().shl(i * 8))
+                }
+                value
+            }
+        }
+    }
+
+    fun readULong(offset: ULong): ULong{
+        return ByteArray(8).let {
+            buffer.atOffset(offset) {
+                buffer.get(it)
+                var offset = 0UL
+                for (i in 0 until 8) {
+                    offset = offset.or(it[i].toUByte().toULong().shl(i * 8))
+                }
+                offset
+            }
+        }
+    }
+
+    fun section(sectionName: String): ElfSectionHeader? {
+        val sectionNumber = readShort(bitness.sectionHeaderNumber.toULong())
+        for (i in 0 until sectionNumber) {
+            val section = section(i)
+            if (sectionHeaderStringTable.readString(section.nameIndex.toInt()) == sectionName) {
+                return section
+            }
+        }
+        return null
+    }
+
+    fun section(index: Int): ElfSectionHeader {
+        val offset = sectionHeaderOffset + (sectionHeaderEntrySize * index).toUInt()
+        val typeOffset = offset + 4u;
+        val type = readUInt(typeOffset)
+        return when(type) {
+            1u -> ElfProgBitsSectionHeader(offset)
+            3u -> ElfStrTabSection(offset)
+            else -> ElfSectionHeader(offset)
+
+        }
+    }
+
+    open inner class ElfSectionHeader(
+        val offset: ULong
+    ) {
+        val nameIndex = readUInt(offset)
+        val type = readUInt(offset + 4u)
+        val sectionOffset = when(bitness) {
+            BitnessHeaderOffsets.BITNESS_32 -> readUInt(offset + 16u).toULong()
+            BitnessHeaderOffsets.BITNESS_64 -> readULong(offset + 24u)
+        }
+        val sectionSize = when(bitness) {
+            BitnessHeaderOffsets.BITNESS_32 -> readUInt(offset + 20u).toULong()
+            BitnessHeaderOffsets.BITNESS_64 -> readULong(offset + 32u)
+        }
+    }
+
+    open inner class ElfProgBitsSectionHeader(offset: ULong) : ElfSectionHeader(offset)
+    inner class ElfStrTabSection (offset: ULong) : ElfSectionHeader(offset) {
+        /*constructor(header: ElfSectionHeader) : this(header.offset) {
+            if (type != 0x3.toUInt())
+                throw IllegalStateException("$type isn't string tab 0x3")
+        }*/
+        constructor(header: ElfProgBitsSectionHeader) : this(header.offset)
+        fun readString(index : Int): String {
+            return buffer.atOffset((sectionOffset + index.toUInt())) {
+                val bytes = mutableListOf<Byte>()
+                var b: Byte
+                while(true){
+                    b = buffer.get()
+                    if (b == 0.toByte())
+                        break
+                    bytes.add(b)
+                }
+                String(bytes.toByteArray())
+            }
+        }
+        fun dumpTable(): List<String> {
+            val dump = mutableListOf<String>()
+            var i = 0
+            while (i < sectionSize.toInt()) {
+                val str = readString(i)
+                if (str.isNotEmpty())
+                    dump.add(str)
+                i += str.length + 1
+            }
+            return dump.toList()
+        }
+    }
+}
+
+inline fun <reified T> ByteBuffer.atOffset(offset: ULong, action:ByteBuffer.() -> T): T {
+    position(offset.toInt())
+    val v = action()
+    position(0)
+    return v
+}
