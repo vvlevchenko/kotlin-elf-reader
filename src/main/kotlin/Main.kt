01d69f763089213d57a752e4c7e7a829e8d2c910
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2022-11-28 11:52:28 +0100

    [dwarf] resolve api over raw dwarf.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,7 @@ import com.github.vvlevchenko.dwarf.Attribute
 import com.github.vvlevchenko.dwarf.Form
 import com.github.vvlevchenko.dwarf.Tag
 import com.github.vvlevchenko.dwarf.section.*
-import com.github.vvlevchenko.elf.ElfLoader
-import com.github.vvlevchenko.elf.ElfProgBitsSection
-import com.github.vvlevchenko.elf.ElfStrTabSection
-import com.github.vvlevchenko.elf.ElfSymTabSection
+import com.github.vvlevchenko.elf.*
 import java.io.File
 import java.lang.Exception
 import kotlin.system.measureTimeMillis
@@ -89,25 +86,111 @@ fun main() {
         }
     }
     println("dwarf(tags: $tagsCount, attributeCount: $attributeCount) load  $millis in millis")
-    var debugEntry: DebugInfoEntry? = null
-    val lookupMillis = measureTimeMillis {
-        debugEntry = debugInfoSectionSec.find {
-            it.tag == Tag.DW_TAG_class_type
-                && it.attributes.find { it.attribute == Attribute.DW_AT_name }?.let {
-                    val index = when (it) {
-                        is Dwarf32Data -> it.value.toInt()
-                        is Dwarf64Data -> it.value.toInt()
-                        else -> TODO()
-                     }
-                    debugStr?.string(index) == "java.lang.String"
-            } ?: false
-        }
-    }
-    println("java.lang.String(${lookupMillis}): ${debugEntry?.diaOffset?.toString(16)}")
-    loader.section(".debug_types")?.let {
 
-    }
-    debugStr?.let {
-        println("${loader.sectionHeaderStringTable.string(it.nameIndex.toInt())}: ${it.type}")
+    val diClassEnum = debugInfoSectionSec.findClassByName("java.lang.Enum")
+    println("java.lang.Enum(${diClassEnum?.die?.diaOffset?.toString(16)}")
+    val diClassA = debugInfoSectionSec.findClassByName("javagdb.A")
+    diClassA?.run {
+        fields
+        methods
     }
 }
+
+fun DebugInfoSection.findClassByName(name: String):DiClass? {
+    val die = find { die ->
+        die.tag == Tag.DW_TAG_class_type
+                && die.attributes.find { it.attribute == Attribute.DW_AT_name }.let {
+            val index = when (it) {
+                is Dwarf32Data -> it.value.toInt()
+                is Dwarf64Data -> it.value.toInt()
+                else -> TODO()
+            }
+            debugStr(this.debugInfoSection.loader)?.string(index) == name
+        }
+    } ?: return null
+    return DiClass(this, die)
+}
+fun debugStr(loader: ElfLoader) = loader.section(".debug_str")?.let {
+    ElfStrTabSection(loader, it)
+}
+
+fun DebugInfoEntry.attibuteByTag(tag: Tag) = children.find { it.tag == tag }
+fun DebugInfoEntry.attributeDwarfData(attribute: Attribute):Int? {
+    return attributes.find {
+        it.attribute == attribute
+    }?.let {
+        when (it) {
+            is Dwarf32Data -> it.value.toInt()
+            is Dwarf64Data -> it.value.toInt()
+            else -> TODO()
+        }
+    }
+}
+fun DebugInfoSection.findDieByIndex(index: Int) = find { it.diaOffset == index.toULong() }
+
+open class DiType(val debugInfoSection: DebugInfoSection, val die: DebugInfoEntry) {
+    val name:String? by lazy {
+        val index = die.attributeDwarfData(Attribute.DW_AT_name) ?: return@lazy null
+        debugStr(debugInfoSection.debugInfoSection.loader)?.string(index)
+    }
+
+}
+class DiArrayType(debugInfoSection: DebugInfoSection, die: DebugInfoEntry): DiType(debugInfoSection, die)
+class DiBaseType(debugInfoSection: DebugInfoSection, die: DebugInfoEntry): DiType(debugInfoSection, die)
+class DiField(val name: String, val type: DiType)
+class DiParameter(val name: String, val type: DiType)
+class DiMethod(val returnType: DiType, val name: String, vararg val parameters:DiParameter)
+
+class DiClass(debugInfoSection: DebugInfoSection, die: DebugInfoEntry): DiType(debugInfoSection, die) {
+    val superType:DiClass? by lazy {
+        die.attibuteByTag(Tag.DW_TAG_inheritance)?.let { entry ->
+            val index = entry.attributeDwarfData(Attribute.DW_AT_type) ?: return@let null
+            val die = debugInfoSection.findDieByIndex(index) ?: return@let null
+            DiClass(debugInfoSection, die)
+        }
+    }
+
+    val fields by lazy {
+        die.children.filter {
+            it.tag == Tag.DW_TAG_member
+        }.mapNotNull {
+            DiField(nameOfDie(it)!!, typeOfDie(it)!!)
+        }
+    }
+
+    private fun typeOfDie(die: DebugInfoEntry): DiType? {
+        val typeOff = die.attributeDwarfData(Attribute.DW_AT_type) ?: return null
+        val die = debugInfoSection.findDieByIndex(typeOff) ?: return null
+        return when(die.tag) {
+            Tag.DW_TAG_class_type -> DiClass(debugInfoSection, die)
+            Tag.DW_TAG_pointer_type,
+            Tag.DW_TAG_unspecified_type,
+            Tag.DW_TAG_base_type -> DiBaseType(debugInfoSection, die)
+            Tag.DW_TAG_array_type -> DiArrayType(debugInfoSection, die)
+            else -> TODO("${die.tag.name}")
+        }
+    }
+
+    private fun nameOfDie(die: DebugInfoEntry): String? {
+        val nameOff = die.attributeDwarfData(Attribute.DW_AT_name) ?: return null
+        return debugStr(debugInfoSection.debugInfoSection.loader)?.string(nameOff) ?: return null
+
+    }
+
+    val methods by lazy {
+        die.children.filter {
+            it.tag == Tag.DW_TAG_subprogram
+        }.mapNotNull {
+            val name = nameOfDie(it)!!
+            val type = typeOfDie(it)!!
+            val params = it.children.filter {
+                it.tag == Tag.DW_TAG_formal_parameter
+            }.map {
+                DiParameter(nameOfDie(it)!!, typeOfDie(it)!!)
+            }
+            DiMethod(type, name, *params.toTypedArray())
+        }
+    }
+
+}
+
